fc210411bb2ab564c07dd6a00d1b30efd1113552
Aditya patel <apatel89329@gmail.com>
2026-01-05 19:36:22 +0530

    Added Input Validaton

--- Backend/Models/Users.js
+++ Backend/Models/Users.js
@@ -4,9 +4,12 @@ const Schema = mongoose.Schema;
 
 const UserSchema = new Schema({
     name: {type: String, required: true},
-    email: {type: String, required: true},
+    email: {type: String, required: true, unique: true},
     password: {type: String, required: true},
     date: {type: Date, default: Date.now}
 })
+
+UserSchema.index({ email: 1 });
+
 
 export default mongoose.model('user', UserSchema);
--- Backend/Routes/auth.js
+++ Backend/Routes/auth.js
@@ -1,14 +1,41 @@
-import express from 'express';
-import User from '../Models/Users.js';
+import express from "express";
+import User from "../Models/Users.js";
+import { body, validationResult } from "express-validator";
 
 const router = express.Router();
 
 //Create a user using: POST "/api/auth"
 
-router.post('/' , (req, res) =>{
-    const user = User(req.body);
-    user.save();
-    // res.send(req.body);
-})
+router.post(
+  "/",
+  [
+    body('name')
+    .notEmpty().withMessage('Name field should not be empty')
+    .isLength({ min: 3 }).withMessage('Name should be at least 3 characters'),
+    body("email", "Please enter a valid email").isEmail(),
+    body(
+      "password",
+      "Password length should be more than 8 characters"
+    ).isLength({
+      min: 8,
+    }),
+  ],
+  (req, res) => {
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      return res.status(400).json({ errors: errors.array() });
+    }
+
+    User.create({
+      password: req.body.password,
+      name: req.body.name,
+      email: req.body.email,
+    })
+      .then((user) => res.json(user))
+      .catch((err) => {
+        res.send(`Error: ${err}`);
+      });
+  }
+);
 
 export default router;
